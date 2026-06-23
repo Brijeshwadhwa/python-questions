879b18bbf51f7be2f323766aa4bcfa4af1610189
Brijesh Wadhwa <152505034+Brijeshwadhwa@users.noreply.github.com>
2026-06-23 20:23:39 +0530

    Update ques1.2.1.py

--- gokul/week1/lect_1.2/ques1.2.1.py
+++ gokul/week1/lect_1.2/ques1.2.1.py
@@ -8,3 +8,10 @@
 #     *********#
 #     i don't know what to type
 #     ```
+
+# number of rows
+rows = 5
+for i in range(0, rows):
+    for j in range(0, i + 1):
+        print("*", end=' ')
+    print("\r")
