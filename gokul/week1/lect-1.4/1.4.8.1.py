35110eb3b155309b365691723376359275c65e7e
Brijesh Wadhwa <152505034+Brijeshwadhwa@users.noreply.github.com>
2026-06-27 11:40:29 +0530

    Update 1.4.8.1.py

--- gokul/week1/lect-1.4/1.4.8.1.py
+++ gokul/week1/lect-1.4/1.4.8.1.py
@@ -3,10 +3,12 @@ x=input()
 print(type(x))
 # what will be the output if i give "gokul" in command line?
 
-
+<class 'str'>
 
 #  what will be the output if i give 12.2 in command line?
 
-
+<class 'float'>
 
 #  what will be the output if i give 12 in command line
+
+<class 'int'>
