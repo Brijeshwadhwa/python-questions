b77dbf799e29f169b7ef5e0fab699ef63ce63523
Brijesh Wadhwa <152505034+Brijeshwadhwa@users.noreply.github.com>
2026-06-27 11:05:29 +0530

    Update 1.3.8.py

--- gokul/week1/lect-1.3/1.3.8.py
+++ gokul/week1/lect-1.3/1.3.8.py
@@ -7,3 +7,15 @@
 # print["hello"]
 # print<"hello">
 # ```
+
+
+hello
+
+hello
+
+It will gave SyntaxError: Missing parentheses in call to 'print'. Did you mean print(...)?
+
+error : TypeError: 'builtin_function_or_method' object is not subscriptable
+
+
+error :SyntaxError: invalid syntax
