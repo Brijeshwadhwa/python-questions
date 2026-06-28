8a768247ff880499f2f79d174a3629d7112e5f07
Brijesh Wadhwa <152505034+Brijeshwadhwa@users.noreply.github.com>
2026-06-28 17:20:47 +0530

    Update 1.11.3.py

--- gokul/week1/lect-1.11/1.11.3.py
+++ gokul/week1/lect-1.11/1.11.3.py
@@ -18,3 +18,16 @@
 #     print(f)
 #     print(d+e+f)
 #     ```
+
+
+
+print(type(a)) = <class 'str'>
+print(a) = 0
+print(b) = 7 
+print(c) = 4 
+print(a+b+c) = 074
+print("1"+"3"+"6") = 136
+print(d) = 1
+print(e) = 2 
+print(f) = 3 
+print(d+e+f) = 6 
