362d67e1ad8b40b161e449933725f3aeef9278f6
Brijesh Wadhwa <152505034+Brijeshwadhwa@users.noreply.github.com>
2026-06-28 12:48:54 +0530

    Update 1.10.4.py

--- gokul/week1/lect-1.10/1.10.4.py
+++ gokul/week1/lect-1.10/1.10.4.py
@@ -11,3 +11,16 @@
 #     print(7!=3)
 #     print(a=3)
 #     ```
+
+
+
+print(7>3) = True
+print(7<3) = False
+print(7=3) = SyntaxError: expression cannot contain assignment, perhaps you meant "=="?
+print(7==3) = False
+print(7<=7) = True
+print(7<7) = False
+print(7<=3) = False
+print(7>=3) = True
+print(7!=3) = True
+print(a=3) = TypeError: 'a' is an invalid keyword argument for print()
