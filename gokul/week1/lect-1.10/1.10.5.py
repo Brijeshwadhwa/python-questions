1cd6ee8c191968cd87b98f452e6394556aa331cf
Brijesh Wadhwa <152505034+Brijeshwadhwa@users.noreply.github.com>
2026-06-28 12:52:27 +0530

    Update 1.10.5.py

--- gokul/week1/lect-1.10/1.10.5.py
+++ gokul/week1/lect-1.10/1.10.5.py
@@ -11,3 +11,16 @@
 #     print(not(Ture))
 #     print(not(False))
 #     ```
+
+
+
+print(True and True) = True
+print(True and False) = False 
+print(False and False) = False 
+print(False and True) = False 
+print(True or True) = True
+print(True or False) = True
+print(False or False) = False 
+print(False or True) = True
+print(not(Ture)) = NameError: name 'Ture' is not defined
+print(not(False)) = True
