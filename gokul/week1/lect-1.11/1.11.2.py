5cea6cfda0c16bf5bdebe982dd03ec263eb4580d
Brijesh Wadhwa <152505034+Brijeshwadhwa@users.noreply.github.com>
2026-06-28 17:17:31 +0530

    Update 1.11.2.py

--- gokul/week1/lect-1.11/1.11.2.py
+++ gokul/week1/lect-1.11/1.11.2.py
@@ -13,3 +13,18 @@ print(s[1:5])
 print(s-t)
 print(s+t)
 #     ```
+
+
+
+
+
+
+
+print(s[0]) = c
+print(s[1]) = o
+print(s[2]) = f
+print(c[3:7]) = 3456
+print(s[1:4]) = off
+print(s[1:5]) = offe
+print(s-t) = TypeError: unsupported operand type(s) for -: 'str' and 'str'
+print(s+t) = coffeebread
