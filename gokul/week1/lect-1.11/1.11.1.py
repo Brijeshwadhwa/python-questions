9f19884a24540739793d39c32793de6f9a100941
Brijesh Wadhwa <152505034+Brijeshwadhwa@users.noreply.github.com>
2026-06-28 16:43:35 +0530

    Update 1.11.1.py

--- gokul/week1/lect-1.11/1.11.1.py
+++ gokul/week1/lect-1.11/1.11.1.py
@@ -10,3 +10,15 @@
 # print(u*t)
 # print(3.2*"gokul")
 #     ```
+
+
+
+s='coffee'
+t='bread'
+print(s) = cofee
+print(t) = bread
+print(s+t) = cofeebread
+u=3 = TypeError: unsupported operand type(s) for +: 'int' and 'str'
+print(u+s) = NameError: name 'u' is not defined
+print(u*t) = NameError: name 'u' is not defined
+print(3.2*"gokul") = TypeError: can't multiply sequence by non-int of type 'float'
