12004686f6cd6f3454467e83899f4c16c2fe313c
Brijesh Wadhwa <152505034+Brijeshwadhwa@users.noreply.github.com>
2026-07-03 21:53:20 +0530

    Update 1.12.5.py

--- gokul/week1/lect-1.12/1.12.5.py
+++ gokul/week1/lect-1.12/1.12.5.py
@@ -13,3 +13,19 @@ s="python"
 #     print(s[2:100])
 
 #     ```
+
+
+
+
+
+
+print(s[0 ]) = p
+print(s[-1]) = n
+print(s[-2]) = o
+print(s[-3]) = h
+print(s[-4]) = t
+print(s[-5]) = y
+print(s[-6]) = p
+print(s[-7]) = IndexError: string index out of range
+print(s[100]) = IndexError: string index out of range
+print(s[2:100]) = thon
