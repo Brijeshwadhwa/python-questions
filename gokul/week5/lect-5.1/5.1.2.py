8d26ae90548fa43460a19af64e7ec0c054f6f9be
Brijesh Wadhwa <152505034+Brijeshwadhwa@users.noreply.github.com>
2026-07-06 10:48:52 +0530

    Update 5.1.2.py

--- gokul/week5/lect-5.1/5.1.2.py
+++ gokul/week5/lect-5.1/5.1.2.py
@@ -1,1 +1,8 @@
 # create a function named `sub` that takes three parameters as input and  returns their the minimum difference among the three
+
+def sub(a,b,c):
+  return min(abs(a, b), abs(b, c), abs(c, a))
+
+assert sub(1,2,3) == 1
+assert sub(2,3,4) == 1
+assert sub(3,4,5) == 1
