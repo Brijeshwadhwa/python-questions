9218bb1882a34ba193f5b076591b768445df16f7
Brijesh Wadhwa <152505034+Brijeshwadhwa@users.noreply.github.com>
2026-07-06 10:44:44 +0530

    Update 5.1.1.py

--- gokul/week5/lect-5.1/5.1.1.py
+++ gokul/week5/lect-5.1/5.1.1.py
@@ -1,1 +1,8 @@
 # create a function named `add` that takes three parameters as input and  returns their sum as output.
+
+def add(a, b, c):
+  return a+b+c
+
+assert add(1,2,3) == 6
+assert add(2,3,4) == 9
+assert add(3,4,5) == 12
