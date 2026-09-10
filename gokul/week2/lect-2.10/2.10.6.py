802fdc890f4cae9a15e4bf49d57b49348395e03d
Brijesh Wadhwa <152505034+Brijeshwadhwa@users.noreply.github.com>
2026-09-10 10:52:25 +0530

    Update 2.10.6.py

--- gokul/week2/lect-2.10/2.10.6.py
+++ gokul/week2/lect-2.10/2.10.6.py
@@ -6,5 +6,9 @@ print(math.pow(10,0.5))
 print(10**0.5)
 
 # why to use math.pow and 10**0.5
+they both are same and gave the same output. 
+
 #  is there any difference in speed between pow and 10**0.5
+Yes, in Python, the infix operator 10**0.5 is significantly faster than calling the function pow(10, 0.5) because pow() requires an extra function call overhead
+
 # check this out : https://stackoverflow.com/questions/20969773/exponentials-in-python-xy-vs-math-powx-y
