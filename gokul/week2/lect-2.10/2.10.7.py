b190504842f7fd56a3a4ea76f8da165b57f9d506
Brijesh Wadhwa <152505034+Brijeshwadhwa@users.noreply.github.com>
2026-09-10 10:54:00 +0530

    Update 2.10.7.py

--- gokul/week2/lect-2.10/2.10.7.py
+++ gokul/week2/lect-2.10/2.10.7.py
@@ -1,1 +1,9 @@
 # print a random number between 0 to 1 using random library
+
+
+
+import random
+
+# Generate and print a random number between 0 and 1
+random_number = random.random()
+print(random_number)
