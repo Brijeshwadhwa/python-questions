e8967a37e6ef4cd1ed7358224f66db7b3a2ddc6f
Brijesh Wadhwa <152505034+Brijeshwadhwa@users.noreply.github.com>
2026-09-10 10:35:00 +0530

    Update 2.10.10.py

--- gokul/week2/lect-2.10/2.10.10.py
+++ gokul/week2/lect-2.10/2.10.10.py
@@ -3,3 +3,9 @@ import random
 print(random.randrange(1,6))
 
 # will 6 get printed in the console or not?
+
+No, 6 will not get printed in the console.
+random.randrange(start, stop) function in Python includes the start integer but excludes the stop integer. 
+Therefore, random.randrange(1, 6) will only generate numbers from the set {1, 2, 3, 4, 5}.
+If you want to include the number 6, you can use either of these alternatives:
+random.randrange(1, 7)random.randint(1, 6) (The randint function does include the upper limit)
