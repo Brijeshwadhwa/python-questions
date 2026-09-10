d39a4a67b7a1f11bde29847f13df7864ce075ab7
Brijesh Wadhwa <152505034+Brijeshwadhwa@users.noreply.github.com>
2026-09-10 10:33:41 +0530

    Update 2.10.1.py

--- gokul/week2/lect-2.10/2.10.1.py
+++ gokul/week2/lect-2.10/2.10.1.py
@@ -1,1 +1,13 @@
 # how to find the value of log(x) in python, where x is a positive number, answer=2.30....
+
+import math
+a = 10 
+result = math.log(a)
+print(result)
+
+
+
+Other Logarithm Bases in PythonBase 10: 
+Use math.log10(x)Base 2: 
+Use math.log2(x)Custom Base: 
+Use math.log(x, base) (e.g., math.log(x, 5))
