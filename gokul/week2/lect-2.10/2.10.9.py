ae6357c2ff8f6c8550e78c86615dccd06f8eae5d
Brijesh Wadhwa <152505034+Brijeshwadhwa@users.noreply.github.com>
2026-09-11 09:43:00 +0530

    Update 2.10.9.py

--- gokul/week2/lect-2.10/2.10.9.py
+++ gokul/week2/lect-2.10/2.10.9.py
@@ -1,1 +1,11 @@
 # print a random number between 1 and 1000
+
+
+
+
+
+import random
+
+# Generate and print a random integer between 1 and 1000 (inclusive)
+print(random.randint(1, 1000))
+print(random.randrange(1, 1000))
