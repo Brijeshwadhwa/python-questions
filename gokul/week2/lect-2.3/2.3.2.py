ff47bcff7f62ad97b3b9b0159fdff31652160234
Brijesh Wadhwa <152505034+Brijeshwadhwa@users.noreply.github.com>
2026-09-11 09:53:31 +0530

    Update 2.3.2.py

--- gokul/week2/lect-2.3/2.3.2.py
+++ gokul/week2/lect-2.3/2.3.2.py
@@ -4,3 +4,13 @@
 
 
 Python determines a variable's data type automatically at runtime, rather than requiring you to declare it explicitly beforehand.
+
+
+example :
+# Python automatically infers that x is an integer
+x = 42			
+print(type(x))  # Output: <class 'int'>
+
+# Reassigning the same variable to a string works perfectly
+x = "Hello, Python!"	
+print(type(x))  # Output: <class 'str'>
