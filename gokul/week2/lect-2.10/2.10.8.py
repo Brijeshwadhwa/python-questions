cffb4e63203fa2c9c19f691e8c358bd0ab7a88f3
Brijesh Wadhwa <152505034+Brijeshwadhwa@users.noreply.github.com>
2026-09-11 09:46:53 +0530

    Update 2.10.8.py

--- gokul/week2/lect-2.10/2.10.8.py
+++ gokul/week2/lect-2.10/2.10.8.py
@@ -1,1 +1,11 @@
 # simulate a coin toss, if the random number generated is >0.5 , print 'heads' else 'tails'
+
+
+
+import random
+a = random.random()
+print("Random number a:", a)
+if a > 0.5:
+  print("Heads")
+else:
+  print("Tails")
