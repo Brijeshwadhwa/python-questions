4bc9c74dfa1cc2985bd820064d97933b2ec394f0
Brijesh Wadhwa <152505034+Brijeshwadhwa@users.noreply.github.com>
2026-09-14 12:48:24 +0530

    Update 2.4.4.py

--- gokul/week2/lect-2.4/2.4.4.py
+++ gokul/week2/lect-2.4/2.4.4.py
@@ -1,1 +1,11 @@
 # Chaining Operator
+
+
+A chaining operator connects multiple expressions, commands, or conditions in a single line of code.
+
+
+
+age = 25
+
+if age >= 18 and age <= 65:
+    print("Eligible")
