a8000cceab348e896fa3d1eada97389539a6a1ae
Brijesh Wadhwa <152505034+Brijeshwadhwa@users.noreply.github.com>
2026-09-14 12:52:07 +0530

    Update 2.4.5.py

--- gokul/week2/lect-2.4/2.4.5.py
+++ gokul/week2/lect-2.4/2.4.5.py
@@ -3,3 +3,10 @@
 # and all the non keywords in the second line
 
 # and,are,is,not,while,where,for,if,try,catch,except
+
+
+
+
+
+and, is, not, while, for, if, try, exceptare, where, catchExplanationKeywords are predefined, reserved words in Python that have a specific meaning and structural purpose. 
+They cannot be used as regular variable names. [1] (https://brainly.in/question/43421807), [2] (https://www.youtube.com/watch?v=1Q0QwZCK_7o), [3] (https://www.youtube.com/watch?v=_dDN2K7oUes&t=1)Non-keywords from your list include are and where (which hold no special meaning in Python). Additionally, catch is not a Python keyword; Python uses except alongside try to handle exceptions instead of the catch block found in languages like Java or C++
