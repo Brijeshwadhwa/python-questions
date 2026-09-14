2f5b9ee7934891d2bfad1d6f37b7d59bde5d256a
Brijesh Wadhwa <152505034+Brijeshwadhwa@users.noreply.github.com>
2026-09-14 12:33:14 +0530

    Update 2.4.12.py

--- gokul/week2/lect-2.4/2.4.12.py
+++ gokul/week2/lect-2.4/2.4.12.py
@@ -4,3 +4,13 @@ x=10
 print(x)
 del (x)
 print(x)
+
+
+
+10
+ERROR!
+Traceback (most recent call last):
+  File "<main.py>", line 6, in <module>
+NameError: name 'x' is not defined
+
+=== Code Exited With Errors ===
