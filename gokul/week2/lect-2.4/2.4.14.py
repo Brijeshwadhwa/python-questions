b257ac326594e3d8253a083f19429dc68285ad67
Brijesh Wadhwa <152505034+Brijeshwadhwa@users.noreply.github.com>
2026-09-14 12:36:21 +0530

    Update 2.4.14.py

--- gokul/week2/lect-2.4/2.4.14.py
+++ gokul/week2/lect-2.4/2.4.14.py
@@ -3,3 +3,12 @@
 print("alpha" in "i am in alpha squad")
 print("beta" in "i am in betasquad")
 print("gamma" in "i am in gamasquad")
+
+
+
+True
+True
+False
+
+
+Because it is trying to found the word in the sentence.
