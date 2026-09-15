88819db1ce9e98766db7d608c110e863191e125c
Brijesh Wadhwa <152505034+Brijeshwadhwa@users.noreply.github.com>
2026-09-15 12:48:20 +0530

    Update 2.5.1.py

--- gokul/week2/lect-2.5/2.5.1.py
+++ gokul/week2/lect-2.5/2.5.1.py
@@ -2,3 +2,32 @@
 
 # print('it's a beautiful day') 
 # print("we are from "IIT Madras" Madras")
+
+
+
+
+first i run simple as it is 
+
+ERROR!
+Traceback (most recent call last):
+  File "<main.py>", line 2
+    print('it's a beautiful day') 
+                               ^
+SyntaxError: unterminated string literal (detected at line 2)
+
+
+
+ESCAPE CHARATERS : To fix the errors, you must use the backslash (\) as an escape character right before the inner quotation marks so Python reads them as text rather than code syntax.
+
+after fixing with escapce characters : 
+
+
+# Fixed using escape characters
+print('it\'s a beautiful day') 
+print("we are from \"IIT Madras\" Madras")
+
+
+output : 
+
+it's a beautiful day
+we are from "IIT Madras" Madras
