60c0662f294e70fab090aeb434c10007705efb0e
Brijesh Wadhwa <152505034+Brijeshwadhwa@users.noreply.github.com>
2026-09-15 13:02:57 +0530

    Update 2.6.2.py

--- gokul/week2/lect-2.6/2.6.2.py
+++ gokul/week2/lect-2.6/2.6.2.py
@@ -3,3 +3,22 @@ x=input()
 # print true is all characters are lower case
 # print True if all the characters are upper case
 # print True if the string follows the rules of title (all the words have starting letter in capital)
+
+
+
+
+output:
+
+x = input()
+
+# Check if all characters are lowercase
+if x.islower():
+    print(True)
+
+# Check if all characters are uppercase
+elif x.isupper():
+    print(True)
+
+# Check if the string follows title case rules
+elif x.istitle():
+    print(True)
