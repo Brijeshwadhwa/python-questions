c1cc42e1c0b00d46af36768aa7ccd10e7e32b388
Brijesh Wadhwa <152505034+Brijeshwadhwa@users.noreply.github.com>
2026-09-15 12:59:44 +0530

    Update 2.6.10.py

--- gokul/week2/lect-2.6/2.6.10.py
+++ gokul/week2/lect-2.6/2.6.10.py
@@ -7,3 +7,11 @@ x="python is a programming language. python is easy to learn.I love pykkara fals
 print(x.index("python",10))
 print(x.index("python",0,10))
 print(x.index("python",10,20))
+
+
+34
+0
+ERROR!
+Traceback (most recent call last):
+  File "<main.py>", line 6, in <module>
+ValueError: substring not found
