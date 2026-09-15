f27d637af458de0c7b8932f7edfa2bbb49e54fcf
Brijesh Wadhwa <152505034+Brijeshwadhwa@users.noreply.github.com>
2026-09-15 12:57:36 +0530

    Update 2.6.1.py

--- gokul/week2/lect-2.6/2.6.1.py
+++ gokul/week2/lect-2.6/2.6.1.py
@@ -15,3 +15,40 @@ x="PytHON StRIng METhodS"
 
 
 # swap the case of the string
+
+
+
+
+
+Solution:
+
+
+# do the following operations
+
+x="PytHON StRIng METhodS"
+
+# convert the string in lower case
+print(x.lower())
+
+# convert the string in Upper case
+print(x.upper())
+
+# convert the first character to upper case
+print(x.capitalize())
+
+# convert first character of each word to upper case
+
+print(x.title())
+# swap the case of the string
+print(x.swapcase())
+
+
+
+
+output :
+
+python string methods
+PYTHON STRING METHODS
+Python string methods
+Python String Methods
+pYThon sTriNG metHODs
