2114dfc3f8c003debc54814b297db78688a74b27
Brijesh Wadhwa <152505034+Brijeshwadhwa@users.noreply.github.com>
2026-09-15 12:43:06 +0530

    Update 2.4.7.py

--- gokul/week2/lect-2.4/2.4.7.py
+++ gokul/week2/lect-2.4/2.4.7.py
@@ -5,3 +5,16 @@
 # c) _a=10
 # d) $a=10
 # e) A b=10
+
+
+
+
+
+a) 1a=10: Invalid.
+Variable names cannot begin with a number or digit.
+b) 1a1=10: Invalid. 
+Like option (a), it begins with a digit, which violates standard naming conventions.
+d) $a=10: Invalid 
+in standard languages like Python and C because it contains the special character $. 
+(Note: It is considered valid in web-based languages like PHP, JavaScript, or bash).e) A b=10: Invalid.
+Variable names must be continuous words and cannot contain blank spaces.
