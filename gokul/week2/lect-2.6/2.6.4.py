852d02c0985f1f7c1a2a7ada597986b19a516dcc
Brijesh Wadhwa <152505034+Brijeshwadhwa@users.noreply.github.com>
2026-09-17 09:32:54 +0530

    Update 2.6.4.py

--- gokul/week2/lect-2.6/2.6.4.py
+++ gokul/week2/lect-2.6/2.6.4.py
@@ -13,3 +13,24 @@ x="      python       "
 
 
 
+
+
+
+To remove spaces in a string we use the strip method
+
+
+x="      python       "
+
+# remove all the white spaces in the string from both side
+
+print(x.strip())
+
+# remove all the white spaces in the string from right side
+
+print(x.rstrip())
+
+# remove all the white spaces in the string from left side
+
+
+print(x.lstrip())
+
