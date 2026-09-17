2af44ee9bea3a66068db9654c492b7638ac143e8
Brijesh Wadhwa <152505034+Brijeshwadhwa@users.noreply.github.com>
2026-09-17 16:53:47 +0530

    Update 2.6.9.py

--- gokul/week2/lect-2.6/2.6.9.py
+++ gokul/week2/lect-2.6/2.6.9.py
@@ -1,3 +1,21 @@
 # replace all the occurence of 'py' with 'python' in the given string
 
 x="python is a programming language. python is easy to learn.I love pykkara false"
+
+print(x.replace("pi", "python")
+
+
+
+
+
+
+
+output: 
+
+x = "python is a programming language. python is easy to learn.I love pykara falls"
+
+# Attempting to replace 'pi' with 'python'
+new_string = x.replace('pi', 'python')
+
+print(new_string)
+# Output: "python is a programming language. python is easy to learn.I love pykara falls"
