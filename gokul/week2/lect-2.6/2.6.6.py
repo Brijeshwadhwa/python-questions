c72933c9fade1bbe71a56c3763cdc18e9b77f550
Brijesh Wadhwa <152505034+Brijeshwadhwa@users.noreply.github.com>
2026-09-17 09:44:09 +0530

    Update 2.6.6.py

--- gokul/week2/lect-2.6/2.6.6.py
+++ gokul/week2/lect-2.6/2.6.6.py
@@ -2,3 +2,23 @@
 
 x=input()
 
+
+
+
+my solution:
+
+
+print("Write a string: ")
+x = input()
+
+# Fixed: Added quotes around "I" and "N", and changed 'or' to 'and'
+if x[0] == "I" and x[-1] == "N":
+    print("True")
+else:
+    print("False")
+
+
+GEMINI SOLUTION:
+
+x = input()
+print(x.startswith("I") and x.endswith("N"))
