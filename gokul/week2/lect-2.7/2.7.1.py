4e40835c2dd71a0675a692685d6a21cbcf0d60c5
Brijesh Wadhwa <152505034+Brijeshwadhwa@users.noreply.github.com>
2026-09-18 12:00:30 +0530

    Update 2.7.1.py

--- gokul/week2/lect-2.7/2.7.1.py
+++ gokul/week2/lect-2.7/2.7.1.py
@@ -6,3 +6,18 @@ i=8
 print(x[i])
 print(x[-i])
 print(x[i+26])
+
+
+
+
+
+Output:
+
+i
+s
+ERROR!
+Traceback (most recent call last):
+  File "<main.py>", line 5, in <module>
+IndexError: string index out of range
+
+=== Code Exited With Errors ===
