1b99de0b00d1cb5d1733b65c1e5e24f55f94040e
Brijesh Wadhwa <152505034+Brijeshwadhwa@users.noreply.github.com>
2026-09-18 12:11:39 +0530

    Update 2.7.3.py

--- gokul/week2/lect-2.7/2.7.3.py
+++ gokul/week2/lect-2.7/2.7.3.py
@@ -8,3 +8,19 @@ print(alpha[i+1])
 print(alpha[i+2])
 print(alpha[i+2]%26)
 print(alpha[(i+2)%26])
+
+
+output:
+
+
+z
+
+Traceback (most recent call last):
+  File "<main.py>", line 8, in <module>
+IndexError: string index out of range
+
+Traceback (most recent call last):
+  File "<main.py>", line 9, in <module>
+IndexError: string index out of range
+
+a
